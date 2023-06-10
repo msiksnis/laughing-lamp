c644f6b7125ab8379e8d3c47d1437a024169cfd4
Martins Siksnis <msiksnis@gmail.com>
2023-06-10 14:16:06 +0200

    ui update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
       <div className="text-4xl font-bold text-gray-800 py-10">Home Page</div>
-      <Link href="/dashboard" className="text-lg">
+      <Link href="/dashboard" className="text-xl">
         Go to Dashboard
       </Link>
     </div>
