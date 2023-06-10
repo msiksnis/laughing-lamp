c7635acfe86209f306a6a2b876967786f67a325c
Martins Siksnis <msiksnis@gmail.com>
2023-06-10 14:09:45 +0200

    modified homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,12 @@
-import { useState } from "react";
-import TreatmentItem from "@/components/dashboard/TreatmentItem";
-import { fetchServices } from "../../utils/fetchServices";
-import { Reorder } from "framer-motion";
+import Link from "next/link";
 
-export default function HomePage({ services: initialServices }) {
-  const [services, setServices] = useState(initialServices);
-
+export default function HomePage() {
   return (
-    <main className="m-10">
-      <h1 className="text-xl mb-10">All Services</h1>
-      <Reorder.Group axis="y" onReorder={setServices} values={services}>
-        {services.map((service) => (
-          <TreatmentItem key={service._id} service={service} />
-        ))}
-      </Reorder.Group>
-    </main>
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <div className="text-4xl font-bold text-gray-800 py-10">Home Page</div>
+      <Link href="/dashboard" className="text-lg">
+        Go to Dashboard
+      </Link>
+    </div>
   );
-}
-
-export async function getServerSideProps() {
-  const { data: services } = await fetchServices();
-
-  return {
-    props: {
-      services,
-    },
-  };
 }
