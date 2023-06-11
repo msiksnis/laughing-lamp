235c735994a4dce26cd4b0442ee953085c16299f
Martins Siksnis <msiksnis@gmail.com>
2023-06-11 19:51:52 +0200

    some ui updates for sign in page

---
+++ src/pages/dashboard/gift-cards.js
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+
+export default function GiftCrdsPage() {
+  return (
+    <div className="flex justify-center mt-52 h-screen">
+      <h1 className="text-xl">Coming Soon</h1>
+    </div>
+  );
+}
+
+GiftCrdsPage.layout = DashboardLayout;
---
+++ src/pages/dashboard/packages.js
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+
+export default function PackagesPage() {
+  return (
+    <div className="flex justify-center mt-52 h-screen">
+      <h1 className="text-xl">Coming Soon</h1>
+    </div>
+  );
+}
+
+PackagesPage.layout = DashboardLayout;
