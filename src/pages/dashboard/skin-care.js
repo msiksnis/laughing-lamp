969e4eb4843480183fa123270cde1d197b416755
Martins Siksnis <msiksnis@gmail.com>
2023-06-26 12:07:21 +0200

    added mt for spinner loader

--- src/pages/dashboard/skin-care.js
+++ src/pages/dashboard/skin-care.js
@@ -38,7 +38,7 @@ export default function SkinCarePage({ initialSkinCareServices, categories }) {
 
   if (isValidating) {
     return (
-      <div className="flex justify-center items-center mb-[70vh] h-screen w-full">
+      <div className="flex justify-center translate-y-1/3 h-screen w-full">
         <Oval
           ariaLabel="loading-indicator"
           height={80}
